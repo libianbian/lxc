7a1a868c3e3a28a13b722b47dcde11726b4b765d
liyuan <liyuang@qihangjinfu.com>
2021-03-31 18:23:42 +0800

    测试

--- src/main/java/com/example/liyuan/sorter/EntityTest.java
+++ src/main/java/com/example/liyuan/sorter/EntityTest.java
@@ -7,9 +7,13 @@ public class EntityTest {
     private String nMetType;
 
     public static void main(String[] args) {
-        EntityTest entityTest = new EntityTest();
-        entityTest.getNMetType();
+//        EntityTest entityTest = new EntityTest();
+//        entityTest.getNMetType();
 //        entityTest.getnMetType();
+        String s = "0|20210312|095355|3|EBB0001000226530|2|310899991010008776083|01310150999自动化B5467872|C|CNY|1000.00|-13799551.26|0|310899999700008327691|01310150999自动化B5467872||01310401999||||||||";
+        String[] arr = s.split("\\|",25);
+        System.out.println(s.substring(0,7));
+
     }
 }
 
